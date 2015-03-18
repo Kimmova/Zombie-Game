e522ed1c6b023967d3698bddaa66031614b13d4c
kimmova <kimmoandersen@Kimmos-MBP.ucn.moc>
2015-03-18 10:22:24 +0100

    Moved
    Moved follow camera functionality to the camera object

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject objectToFollow;
+
+
+	void Update () {
+		var newCamPos = objectToFollow.transform.position;
+		newCamPos.z = transform.position.z;
+		
+		transform.position = newCamPos;
+	}
+}
