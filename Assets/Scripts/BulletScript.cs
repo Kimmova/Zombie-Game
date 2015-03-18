0717fc82338582ba99a88a39a02d8ebbc2a5fd43
kimmova <kimmoandersen@Kimmos-MBP.ucn.moc>
2015-03-18 12:39:30 +0100

    Added collision

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class BulletScript : MonoBehaviour {
 
 	public float speed;
-	void Start () {
-		rigidbody2D.velocity = transform.up * speed;
-	}
 
 	void Update(){
-
+		transform.position += transform.up * speed * Time.deltaTime;
 	}
-	
 
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Enemy") {
+			Destroy (col.gameObject);
+			Destroy (gameObject);
+		}
+	}
 }
