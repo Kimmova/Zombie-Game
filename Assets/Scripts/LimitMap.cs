11a77913022b8388e89252735d2124a23adc548f
kimmova <kimmoandersen@Kimmos-MacBook-Pro.local>
2015-03-20 12:47:52 +0100

    First attempt at collision
    Collision for limit map

---
+++ Assets/Scripts/LimitMap.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitMap : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Bullet") {
+			Destroy (col.gameObject);
+		}
+	}
+}
