b1a6b4ffd6e9211d28caa55c559f810d97857d84
procass <mkkjaer-tillort@hotmail.com>
2015-03-20 11:28:57 +0100

    BloodScript added
    Jeps

---
+++ Assets/Scripts/BloodScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodScript : MonoBehaviour {
+
+	public float bloodTimer;
+	public float time ;
+
+	void Start(){
+		InvokeRepeating ("Deteriorate", 0, time);
+	}
+
+	void Deteriorate(){
+
+		Color color = renderer.material.color;
+		color.a -= 100 / bloodTimer / 100;
+		renderer.material.color = color;
+		if (color.a <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
