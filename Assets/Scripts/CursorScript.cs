6c99309befa2e0c9eb93d406c73e1b10ee1f07e4
procass <mkkjaer-tillort@hotmail.com>
2015-03-26 13:58:03 +0100

    Cursor changed!
    Looks great!

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class CursorScript : MonoBehaviour {
+
+	public Texture2D crosshair;
+	int cursorSizeX = 64;
+	int cursorSizeY = 64;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	public void OnGUI(){
+
+		GUI.DrawTexture(new Rect(Event.current.mousePosition.x - cursorSizeX/2, Event.current.mousePosition.y - cursorSizeY/2, cursorSizeX, cursorSizeY), crosshair);
+
+	}
+}
